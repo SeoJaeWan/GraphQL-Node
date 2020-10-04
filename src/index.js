33b8360539aa98b7e79d65203cd63fa9bbab9cc5
SeoJaeWan <sjw7324@naver.com>
2020-10-04 11:57:37 +0900

    GraphQL Prisma Query, Mutation

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
+const { prisma } = require("./generated/prisma-client");
 const { GraphQLServer } = require("graphql-yoga");
 
-// 1
-const typeDefs = `
-    type Query {
-        info: String!
-    }
-`;
-// 2
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
+
 const resolvers = {
-  Query: {
-    info: () => null,
-  },
+  Query,
+  Mutation,
+  User,
+  Link,
 };
 
-// 3
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: "./src/lib/schema.graphql",
   resolvers,
+  context: (request) => {
+    return {
+      ...request,
+      prisma,
+    };
+  },
 });
 
 server.start(() => console.log(`http://localhost:4000에서 서버 가동중!`));
